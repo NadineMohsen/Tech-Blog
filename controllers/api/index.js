8a27bc025481a3d6bd3ef24246cd57e6af340323
Nadine <nadine.mohsen@hotmail.com>
2022-04-01 16:55:41 +0200

    api index.js

---
+++ controllers/api/index.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+const userRoutes = require('./userRoutes');
+const postRoutes = require('./projectRoutes');
+const commentRoutes = require('./commentRoutes');
+
+router.use('/users', userRoutes);
+router.use('/projects', projectRoutes);
+router.use('/comments', commentRoutes);
+
+
+module.exports = router;
